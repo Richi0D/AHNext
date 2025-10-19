7efbcf7211a0750309fe3714fb70862b9dba65dd
Richi0D <46872517+Richi0D@users.noreply.github.com>
2025-10-19 09:30:38 +0200

    Initial commit

---
+++ AntennaHelperNext/AHEditor.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using KSP.Localization;
+using ToolbarControl_NS;
+
+namespace AntennaHelperNext
+{
+    [KSPAddon(KSPAddon.Startup.EditorAny, false)]
+    public class AHEditor : MonoBehaviour
+    {
+
+    }
+}
